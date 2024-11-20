6a361bcac0ee29c8648489237e31c491fc4ffa7f
D-Hagmann <dominik.hagmann@me.com>
2024-11-20 19:06:44 +0000

    updating ALU

--- ALU.sv
+++ ALU.sv
@@ -7,16 +7,19 @@ module ALU #(
     output logic EQ
 );
 
-    always @() 
-        begin
-        if(ALUop1 == ALUop2) 
-            EQ = 1'b1;
-        else 
-            EQ = 1'b0;
+    always_ff() begin
+
+      
+        EQ = 1'b0;
     
         case(ALUctrl)
         3'b000: ALUResult = ALUop1 + ALUop2;
-
+        3'b001: begin
+            if(ALUop1 == ALUop2) 
+                EQ = 1'b1;
+            else 
+                EQ = 1'b0;
+        end
 
         default: ALUResult = 32'b0;
     end
