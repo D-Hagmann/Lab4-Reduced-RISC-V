0306c97260a0170687005f046b98ce997c667b90
aw-999 <masteraw.999@gmail.com>
2024-11-24 19:40:27 +0000

    fixed program counter

--- toptask1.sv
+++ toptask1.sv
@@ -2,7 +2,6 @@ module toptask1# (
     parameter WIDTH = 32,
 )(
     //pcsrc
-    input logic [DATA_WIDTH-1:0] inc_PC,
     input logic [DATA_WIDTH-1:0] PC_i,
     input logic [DATA_WIDTH-1:0] ImmOp,
     input logic PCsrc,
@@ -17,10 +16,10 @@ module toptask1# (
     logic [DATA_WIDTH-1:0] next_PC;
 
 pcsrc PCMUX (
-    .inc_PC (inc_PC),
     .PC_i (PC_i),
     .ImmOp (ImmOp),
     .PCsrc (PCsrc)
+    .next_PC(next_PC)
 );
 
 pc PC (
